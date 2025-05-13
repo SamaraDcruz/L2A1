afae395bfffe748a99e34101758cdecbf99d40f1
SamaraDcruz <samaragracedcruz@gmail.com>
2025-05-13 01:49:10 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+name = "Samara"
+age = 12
+is_student = True
+weight = 38.5
+
+print("Name :", name)
+print("Data Type of name is,", type (name))
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+
+
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+
+
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
